5226867008021eafdf0ce99f00c603ba9e7a0e7d
sk8rdev <sk8r.development@gmail.com>
2016-11-26 11:54:39 +0100

    added all my tweaks

--- packages.sh
+++ packages.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #dpkg-deb -bZgzip projects/nctextcolor/.theos/_/ debs
 cp projects/com.sk8r.nctextcolor.deb debs
+cp projects/com.sk8r.tweakify.deb debs
+cp projects/com.sk8r.lightify.deb debs
+cp projects/com.sk8r.spotifylyrics.deb debs
