62a53cea620fc382e5774ad17d1ed969ef7dffa1
sk8rdev <sk8r.development@gmail.com>
2016-11-26 12:41:33 +0100

    added all my tweaks

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #dpkg-deb -bZgzip projects/nctextcolor/.theos/_/ debs
 cp projects/com.sk8r.nctextcolor_1.0.1_iphoneos-arm.deb debs
-cp projects/com.sk8r.tweakify.deb debs
-cp projects/com.sk8r.lightify.deb debs
-cp projects/com.sk8r.spotifylyrics.deb debs
+cp projects/com.sk8r.tweakify_0.1_iphoneos-arm.deb debs
+cp projects/com.sk8r.lightify_1.0.1_iphoneos-arm.deb debs
+cp projects/com.sk8r.spotifylyrics_0.0.1_iphoneos-arm.deb debs
