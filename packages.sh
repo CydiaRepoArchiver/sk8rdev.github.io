90c5cad0a32acac8ef29b677c91da5453d009b24
sk8rdev <sk8r.development@gmail.com>
2016-11-26 12:18:03 +0100

    added NCTextColor

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #dpkg-deb -bZgzip projects/nctextcolor/.theos/_/ debs
-cp projects/com.sk8r.nctextcolor.deb debs
+cp projects/com.sk8r.nctextcolor_1.0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.tweakify.deb debs
 cp projects/com.sk8r.lightify.deb debs
 cp projects/com.sk8r.spotifylyrics.deb debs
