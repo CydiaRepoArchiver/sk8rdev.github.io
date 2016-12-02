1ac8c90c040188501cf6b1a4d0dd417641613a9d
sk8rdev <sk8r.development@gmail.com>
2016-12-02 15:48:31 +0100

    added new tweak 'WinkLock' that fixes bug of 'Wink' where it wouldn't work on lock screen

--- packages.sh
+++ packages.sh
@@ -4,3 +4,4 @@ cp projects/com.sk8r.nctextcolor_1.0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.tweakify_0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.lightify_1.0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.spotifylyrics_0.0.1_iphoneos-arm.deb debs
+cp projects/com.sk8r.winklock_0.0.1_iphoneos-arm.deb debs
