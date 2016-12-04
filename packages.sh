b3232f96348e13f1fb2c22b8590f7765468367cd
sk8rdev <sk8r.development@gmail.com>
2016-12-04 20:32:42 +0100

    updated WinkLock

--- packages.sh
+++ packages.sh
@@ -4,5 +4,5 @@ cp projects/com.sk8r.nctextcolor_1.0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.tweakify_0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.lightify_1.0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.spotifylyrics_0.0.1_iphoneos-arm.deb debs
-cp projects/com.sk8r.winklock_0.0.1_iphoneos-arm.deb debs
+cp projects/com.sk8r.winklock_0.1_iphoneos-arm.deb debs
 cp projects/com.sk8r.glimpse_0.0.1_iphoneos-arm.deb debs
